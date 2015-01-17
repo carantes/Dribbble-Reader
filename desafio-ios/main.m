9decae378dd1d3fc309b986998448ba422ea50df
Carlos Eduardo Arantes Ferreira <Dudu@Carloss-MacBook.local>
2015-01-16 22:14:05 -0200

    Empty Project

---
+++ desafio-ios/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  desafio-ios
+//
+//  Created by Carlos Eduardo Arantes Ferreira on 16/01/15.
+//  Copyright (c) 2015 Mobistart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
